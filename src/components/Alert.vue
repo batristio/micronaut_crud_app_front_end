d07c999cff5fd946e686825b3e81ba8cf780f211
Chris Krastevski <chris.krastevski@qualsafeawards.org>
2019-05-24 15:11:39 +0100

    Initial commit

---
+++ src/components/Alert.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <b-alert show>Default Alert</b-alert>
+
+        <b-alert variant="success" show>Success Alert</b-alert>
+
+        <b-alert v-model="showDismissibleAlert" variant="danger" dismissible>
+            Dismissible Alert!
+        </b-alert>
+
+        <b-alert
+                :show="dismissCountDown"
+                dismissible
+                variant="warning"
+                @dismissed="dismissCountDown=0"
+                @dismiss-count-down="countDownChanged"
+        >
+            <p>This alert will dismiss after {{ dismissCountDown }} seconds...</p>
+        </b-alert>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                dismissSecs: 10,
+                dismissCountDown: 0,
+                showDismissibleAlert: false
+            }
+        },
+        methods: {
+            countDownChanged(dismissCountDown) {
+                this.dismissCountDown = dismissCountDown
+            },
+            showAlert() {
+                this.dismissCountDown = this.dismissSecs
+            }
+        }
+    }
+</script>
---
+++ src/components/Person.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="hello">
+        <div>
+            <ul v-for="person in people" v-bind:key="person">
+                <li>First Name: {{ person.firstName }}</li>
+                <li>Last Name: {{ person.lastName }}</li>
+                <li>Age: {{ person.age }}</li>
+                <li>Id: {{ person.id }}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+
+    name: 'Person',
+    data() {
+        return {
+            people:[]
+        };
+    },
+    created: function () {
+        axios.get('http://192.168.1.173:8086/person/list/')
+            .then(response => {
+            this.people = response.data;
+        });
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
