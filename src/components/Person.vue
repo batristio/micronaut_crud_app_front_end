d0d993e2ad98c9003a753c0cdfd25d278d92d9f4
Hristo Krastevski <H.S.Krastevski@bradford.ac.uk>
2019-05-28 08:26:45 +0100

    WIP: *Bootstrap integration

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,11 +1,20 @@
 <template>
     <div class="hello">
-        <div v-for="(person, i) in people" v-bind:key="person">
+        <!--<div v-for="(person, i) in people" v-bind:key="i">
             <span>First Name: {{ person.firstName }}</span><br/>
             <span>Last Name: {{ person.lastName }}</span><br/>
             <span>Age: {{ person.age }}</span><br/>
             <span>Id: {{ person.id }}</span><br/>
             <button v-on:click="deletePerson(person.id, i)">x</button><br/>
+        </div>-->
+        <div>
+            <b-table striped hover :items="people" :fields="fields">
+                <template slot="show_details">
+                    <b-button size="sm" @click="deletePerson(row.id, data.index)" class="mr-2">
+                        x
+                    </b-button>
+                </template>
+            </b-table>
         </div>
     </div>
 </template>
@@ -19,12 +28,18 @@ export default {
     name: 'Person',
     data() {
         return {
-            people:[]
+            people : [],
+            fields : [
+                { firstName    : { sortable: true } },
+                { lastName     : { sortable: true } },
+                { age          : { sortable: true } },
+                { deletePerson : { label: "Delete" } }
+            ],
         };
     },
     created: function () {
         axios
-            .get('http://192.168.1.173:8086/person/list/')
+            .get('http://localhost:8086/person/list')
             .then(response => {
             this.people = response.data;
         });
@@ -32,7 +47,7 @@ export default {
     methods: {
         deletePerson(id, i) {
             axios
-                .delete('http://192.168.1.173:8086/person/delete/' + id)
+                .delete('http://localhost:8086/person/delete/' + id)
                 .then(() => {
                     this.people.slice(i, 1);
                 });
